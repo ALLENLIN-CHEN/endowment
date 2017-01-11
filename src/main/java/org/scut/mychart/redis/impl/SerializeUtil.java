8805b72a7cd7792f8b94699bb37560f5dc6cda94
xiao546 <670775295@qq.com>
2017-01-11 14:28:29 +0800

    修改了一下

---
+++ src/main/java/org/scut/mychart/redis/impl/SerializeUtil.java
@@ -0,0 +1,40 @@
+package org.scut.mychart.redis.impl;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * Created by 546 on 2017/1/4.
+ */
+public class SerializeUtil {
+    public static byte[] serialize(Object object){
+        ObjectOutputStream oos = null;
+        ByteArrayOutputStream baos = null;
+        try{
+            //序列化
+            baos = new ByteArrayOutputStream();
+            oos = new ObjectOutputStream(baos);
+            oos.writeObject(object);
+            byte[] bytes = baos.toByteArray();
+            return bytes;
+        }catch (Exception e){
+
+        }
+        return null;
+    }
+
+    public static Object unserialize(byte[] bytes){
+        ByteArrayInputStream bais = null;
+        try{
+            //反序列化
+            bais = new ByteArrayInputStream(bytes);
+            ObjectInputStream ois = new ObjectInputStream(bais);
+            return ois.readObject();
+        }catch (Exception e){
+
+        }
+        return null;
+    }
+}
